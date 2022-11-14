6b7cbffaeac3264f279c3cf1b69d02a66b55657b
Vladimir Smelov <vladimirfol@gmail.com>
2022-11-15 01:36:54 +0800

    SolidoNFTMarketplaceV2.sol deploy to emerland oasis

--- contracts/SolidoNFTMarketplace.sol
+++ contracts/SolidoNFTMarketplace.sol
@@ -1,119 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
+import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 import '@openzeppelin/contracts/utils/structs/EnumerableSet.sol';
-import "./SolidoGenesisNFT.sol";
-
-// inspired by https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/structs/EnumerableSet.sol
-library ListingSetLib {
-    struct Listing {
-        address nftContract;
-        uint256 tokenId;
-        address payableToken;
-        uint256 price;
-    }
-
-    struct ListingSet {
-        Listing[] _values;
-        // it's not possible to use struct Listing as a mapping key so we use embedded mappings
-        //   per each attribute
-        //   think about the following structure as about
-        //   mapping(Listing /*value*/ => uint256 /*index*/)
-        mapping(address /*nftContract*/ =>
-            mapping(uint256 /*tokenId*/ =>
-                mapping(address /*payableToken*/ =>
-                    mapping(uint256 /*price*/ => uint256 /*index*/)))) _indexes;
-    }
-
-    function getIndex(
-        ListingSet storage set,
-        Listing memory value
-    ) private view returns(uint256) {
-        return set._indexes[value.nftContract][value.tokenId][value.payableToken][value.price];
-    }
-
-    function setIndex(
-        ListingSet storage set,
-        Listing memory value,
-        uint256 index
-    ) private {
-        set._indexes[value.nftContract][value.tokenId][value.payableToken][value.price] = index;
-    }
-
-    function deleteIndex(
-        ListingSet storage set,
-        Listing memory value
-    ) private {
-        delete set._indexes[value.nftContract][value.tokenId][value.payableToken][value.price];
-    }
-
-    function add(
-        ListingSet storage set,
-        Listing memory value
-    ) internal {
-        if (!contains(set, value)) {
-            set._values.push(value);
-            // The value is stored at length-1, but we add 1 to all indexes
-            // and use 0 as a sentinel value
-            setIndex(set, value, set._values.length);
-        }
-    }
-
-    function remove(
-        ListingSet storage set,
-        Listing memory value
-    ) internal {
-        // We read and store the value's index to prevent multiple reads from the same storage slot
-        uint256 valueIndex = getIndex(set, value);
-
-        if (valueIndex != 0) {
-            // Equivalent to contains(set, value)
-            // To delete an element from the _values array in O(1), we swap the element to delete with the last one in
-            // the array, and then remove the last element (sometimes called as 'swap and pop').
-            // This modifies the order of the array, as noted in {at}.
-
-            uint256 toDeleteIndex = valueIndex - 1;
-            uint256 lastIndex = set._values.length - 1;
-
-            if (lastIndex != toDeleteIndex) {
-                Listing memory lastValue = set._values[lastIndex];
-
-                // Move the last value to the index where the value to delete is
-                set._values[toDeleteIndex] = lastValue;
-                // Update the index for the moved value
-                setIndex(set, lastValue, valueIndex); // Replace lastValue's index to valueIndex
-            }
-
-            // Delete the slot where the moved value was stored
-            set._values.pop();
-
-            // Delete the index for the deleted slot
-            deleteIndex(set, value);
-        }
-    }
-
-    function contains(
-        ListingSet storage set,
-        Listing memory value
-    ) internal view returns (bool) {
-        return getIndex(set, value) != 0;
-    }
-
-    function length(
-        ListingSet storage set
-    ) internal view returns (uint256) {
-        return set._values.length;
-    }
-
-    function at(ListingSet storage set, uint256 index) internal view returns (Listing memory) {
-        return set._values[index];
-    }
-}
+import "./utils/ListingSetLib.sol";
 
 contract SolidoUniversalNFTMarketplace is IERC721Receiver, Ownable {
     using SafeERC20 for IERC20;
@@ -187,9 +83,9 @@ contract SolidoUniversalNFTMarketplace is IERC721Receiver, Ownable {
         uint256 previousPrice;
         (contains, previousPrice) = pricesMap.tryGet(payableToken);  // can be =0
         if (contains) {
-            _listingSet.remove(ListingSetLib.Listing(nftContract, tokenId, payableToken, previousPrice));
+            _listingSet.remove(ListingSetLib.Listing(IERC721(nftContract), tokenId, IERC20(payableToken), previousPrice));
         }
-        _listingSet.add(ListingSetLib.Listing(nftContract, tokenId, payableToken, price));
+        _listingSet.add(ListingSetLib.Listing(IERC721(nftContract), tokenId, IERC20(payableToken), price));
         pricesMap.set(payableToken, price);
         emit NFTPriceSet({
             lister: msg.sender,
@@ -221,7 +117,7 @@ contract SolidoUniversalNFTMarketplace is IERC721Receiver, Ownable {
         (contains, previousPrice) = pricesMap.tryGet(payableToken);  // can be =0
         require(contains, "nothing to clear");
         require(previousPrice > 0, "nothing to clear");
-        _listingSet.remove(ListingSetLib.Listing(nftContract, tokenId, payableToken, previousPrice));
+        _listingSet.remove(ListingSetLib.Listing(IERC721(nftContract), tokenId, IERC20(payableToken), previousPrice));
         pricesMap.remove(payableToken);
 
         emit NFTPriceSet({
@@ -261,7 +157,7 @@ contract SolidoUniversalNFTMarketplace is IERC721Receiver, Ownable {
         for (uint256 index=0; index<item.prices.length; ++index) {
             address payableToken = item.prices[index].payableToken;
             uint256 price = item.prices[index].price;
-            _listingSet.add(ListingSetLib.Listing(item.nftContract, item.tokenId, payableToken, price));
+            _listingSet.add(ListingSetLib.Listing(IERC721(item.nftContract), item.tokenId, IERC20(payableToken), price));
             _nftContractTokenIdPayableTokenPrice[item.nftContract][item.tokenId].set(payableToken, price);
             emit NFTPriceSet({
                 lister: msg.sender,
@@ -301,7 +197,7 @@ contract SolidoUniversalNFTMarketplace is IERC721Receiver, Ownable {
             uint256 price;
             (payableToken, price) = pricesMap.at(0);
             pricesMap.remove(payableToken);
-            _listingSet.remove(ListingSetLib.Listing(nftContract, tokenId, payableToken, price));
+            _listingSet.remove(ListingSetLib.Listing(IERC721(nftContract), tokenId, IERC20(payableToken), price));
             emit NFTPriceSet({
                 lister: msg.sender,
                 nftContract: nftContract,
