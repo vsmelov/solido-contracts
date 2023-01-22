e3f73dc3fbc2b8f4aeb9350ca4f02d8964d84b62
Vladimir Smelov <vladimirfol@gmail.com>
2023-01-23 01:01:21 +0800

    SolidoClaim.sol

--- contracts/SolidoClaim.sol
+++ contracts/SolidoClaim.sol
@@ -6,6 +6,8 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 
 import "./SolidoGenesisNFT.sol";
 
@@ -28,19 +30,51 @@ contract GenesisForwarder is AccessControl {
     }
 }
 
-contract SolidoClaim is Ownable {
+contract SolidoClaim is Ownable, EIP712 {
     using SafeERC20 for IERC20;
+    using SignatureChecker for address;
 
-    event FeeSet(address indexed feeToken, uint256 feeAmount);
-    event FeePaid(address indexed payer, address indexed feeToken, uint256 feeAmount);
+    event FeeSet(IERC20 indexed feeToken, uint256 feeAmount);
+    event FeePaid(address indexed payer, IERC20 indexed feeToken, uint256 feeAmount);
 
-//    mapping (address /*user*/ =>
-//        mapping(address /*nft*/ =>
-//            mapping(uint256 /*tokenId*/ =>
-//                uint256 /*nonce*/))) public nonces;
+    mapping (address /*account*/ =>
+        mapping(IERC721 /*nftContract*/ =>
+            mapping(uint256 /*tokenId*/ =>
+                uint256 /*nonce*/))) public nonces;
+
+    GenesisForwarder public genesisForwarder;
     address public nftHolder;
+    address public verifier;
+
     IERC20 public feeToken;
     uint256 feeAmount;
+
+    constructor(
+        GenesisForwarder _genesisForwarder,
+        address _nftHolder,
+        address _verifier
+    ) EIP712("SolidoClaim", "1") {
+        genesisForwarder = _genesisForwarder;
+        nftHolder = _nftHolder;
+        verifier = _verifier;
+    }
+
+    function claimDigest(
+        address account,
+        IERC721 nftContract,
+        uint256 tokenId,
+        uint256 nonce,
+        uint256 deadline
+    ) public view returns(bytes32 digest){
+        digest = _hashTypedDataV4(keccak256(abi.encode(
+            keccak256("Claim(address account,address nftContract,uint256 tokenId,uint256 nonce,uint256 deadline)"),
+            account,
+            nftContract,
+            tokenId,
+            nonce,
+            deadline
+        )));
+    }
 
     function setFee(IERC20 _feeToken, uint256 _feeAmount) external onlyOwner {
         feeToken = _feeToken;
@@ -53,15 +87,63 @@ contract SolidoClaim is Ownable {
         emit FeePaid(msg.sender, feeToken, feeAmount);
     }
 
-    function claimMintingGenesis(uint256 tokenId) external {
-        _verifyClaim(); // todo
+    function _verifyClaim(
+        bytes memory verifierSignature,
+        address account,
+        IERC721 nftContract,
+        uint256 tokenId,
+        uint256 nonce,
+        uint256 deadline
+    ) internal {
+        require(nonces[account][nftContract][tokenId] == nonce);
+        nonces[account][nftContract][tokenId] = nonce + 1;
+        bytes32 digest = claimDigest({
+            account: account,
+            nftContract: nftContract,
+            tokenId: tokenId,
+            nonce: nonce,
+            deadline: deadline
+        });
+        require(verifier.isValidSignatureNow(digest, verifierSignature), "invalid signature");
+        require(block.timestamp <= deadline, "expired");
+    }
+
+    function claimMintingGenesis(
+        bytes memory verifierSignature,
+        address account,
+        uint256 tokenId,
+        uint256 nonce,
+        uint256 deadline
+    ) external {
+        _verifyClaim({
+            verifierSignature: verifierSignature,
+            account: msg.sender,
+            nftContract: genesisForwarder.genesisNFT(),
+            tokenId: tokenId,
+            nonce: nonce,
+            deadline: deadline
+        });
         _payFee();
         genesisForwarder.mint(msg.sender, tokenId);
     }
 
-    function claim(IERC721 nftAddress, uint256 tokenId) external {
-        _verifyClaim(); // todo
+    function claim(
+        bytes memory verifierSignature,
+        address account,
+        IERC721 nftContract,
+        uint256 tokenId,
+        uint256 nonce,
+        uint256 deadline
+    ) external {
+        _verifyClaim({
+            verifierSignature: verifierSignature,
+            account: msg.sender,
+            nftContract: nftContract,
+            tokenId: tokenId,
+            nonce: nonce,
+            deadline: deadline
+        });
         _payFee();
-        nftAddress.transferFrom(nftHolder, msg.sender, tokenId);
+        nftContract.transferFrom(nftHolder, msg.sender, tokenId);
     }
 }
